e6fd2c92a9d503cb2601c075a44c723448c0a458
ktpmNTPN <ktpm.ntpn@gmail.com>
2021-12-13 10:38:20 +0700

    Push code

--- CharitySocialNetworkDemo/src/main/java/com/bpt/controllers/HomeController.java
+++ CharitySocialNetworkDemo/src/main/java/com/bpt/controllers/HomeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String index(Model model){
-        model.addAttribute("name","Tung");
+        model.addAttribute("name","Phong");
         return "index";
     }
 }
